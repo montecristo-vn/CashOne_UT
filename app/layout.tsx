dfafd60629918966a2356d8c96a972035ec18b8a
montecristo-vn <ng.dao.dung@gmail.com>
2025-10-14 23:28:47 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "CashOne",
+  description: "CashOne - trợ lý ảo của phòng kế toán One Mount.",
   appleWebApp: {
-    title: "NextChat",
+    title: "CashOne",
     statusBarStyle: "default",
   },
 };
