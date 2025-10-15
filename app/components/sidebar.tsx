133caf2998e6af02836b464dd38eb8a0cae4aba8
montecristo-vn <ng.dao.dung@gmail.com>
2025-10-15 10:05:16 +0700

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -261,7 +261,7 @@ export function SideBar(props: { className?: string }) {
         subTitle="Trợ lý ảo của phòng kế toán OneMount"
         logo={
           <Image
-            src="/llm-icons/bot.png"
+            src="/icons/Logo_CashOne.webp"
             alt="CashOne"
             width={40}
             height={40}
